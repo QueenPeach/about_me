669ec84feefc284677f1aac49943210aea729896
Ashley Jordan <ajordanium@gmail.com>
2017-11-09 14:41:07 -0800

    fixed errors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var tally = 0;
+
 var playGame = prompt('Hello! Would you like to play a guessing game?').toLowerCase();
 console.log('guessing game:', playGame);
 
 if(playGame === 'y' || playGame === 'yes') {
   console.log('true', playGame);
   alert('Great! Let\'s get started!');
-  score++;
+  tally++;
 } else {
   console.log('false', playGame);
   alert('Too bad, you\'re playing anyways!');
@@ -18,7 +20,7 @@ console.log('number of chickens:', myChickens);
 if(myChickens === 'y' || myChickens === 'yes') {
   console.log('true', myChickens);
   alert('Correct! If you ever need fresh eggs you know where to ask.');
-  score++;
+  tally++;
 } else {
   console.log('false', myChickens);
   alert('I actually do have 50 chickens, I know, crazy!');
@@ -33,7 +35,7 @@ if(myBirthday === 'y' || myBirthday === 'yes') {
 } else {
   console.log('false', myBirthday);
   alert('It\'s November 17th!');
-  score++;
+  tally++;
 }
 
 var favFood = prompt('Is my favorite food Mexican?').toLowerCase();
@@ -42,7 +44,7 @@ console.log('favorite food:', favFood);
 if(favFood === 'y' || favFood === 'yes') {
   console.log('true', favFood);
   alert('Nachos! Burritos! Tacos! Oh my!');
-  score++;
+  tally++;
 } else {
   console.log('false', favFood);
   alert('Pssh, Mexican food is the best!');
@@ -54,7 +56,7 @@ console.log('favorite color:', favColor);
 if(favColor === 'y', favColor === 'yes') {
   console.log('true', favColor);
   alert('Yes, GRAY IS TOTALLY A COLOR!');
-  score++;
+  tally++;
 } else {
   console.log('false', favColor);
   alert('It is gray, but purple\'s cool too!');
@@ -66,62 +68,61 @@ console.log('my dog:', myCorgi);
 if(myCorgi === 'y' || myCorgi === 'yes') {
   console.log('true', myCorgi);
   alert('Correct! His name is Bodhi!');
-  score++;
+  tally++;
 } else {
   console.log('false', myCorgi);
   alert('I do have a Corgi loaf!');
 }
 
-var num = Math.round(Math.random()* 10);
+var numSiblings = 4;
 console.log('number of siblings:', numSiblings);
 
 var k = 4;
 var wrongNum = false;
 
 while(k > 0) {
-var guessedSib = parseInt(prompt('How many siblings do I have?'));
-console.log('guessed siblings:', guessedSib);
+  var guessedSib = parseInt(prompt('How many siblings do I have?'));
+  console.log('guessed siblings:', guessedSib);
 
-if(guessedSib === num) {
-  alert('You guessed correctly!');
-  tally++;
-  wrongNum = true;
-  break;
-} else if(numSiblings > num) {
-  alert('You guessed too high! Try again.');
-} else if(guessedSib < num) {
-  alert('You guessed too high! Try again.');
-} else {
-  console.log('if done');
-}
-k--;
+  if(guessedSib === numSiblings) {
+    alert('You guessed correctly!');
+    tally++;
+    wrongNum = true;
+    break;
+  } else if(guessedSib > numSiblings) {
+    alert('You guessed too high! Try again.');
+  } else {
+    alert('You guessed too low! Try again.');
+    console.log('if done');
+  }
+  k--;
 }
 if(wrongNum === false) {
   alert('Out of tries!');
 }
 
 
-var statesVisted = ['Oregon', 'Texas', 'California', 'Nebraska'];
-var wrongState = false;
-var o = 6;
+var statesVisited = ['Oregon', 'Texas', 'California', 'Nebraska'];
+var numGuesses = 6;
 
-while(o > 0) {
+while(numGuesses > 0) {
   var guessedState = prompt('Can you guess a state I\'ve visited?');
-  if(states.includes(guessedState));
-  var index = states.indexOf(guessedState);
-  states.splice(index);
-  alert('Good job! The others were ‘ + states[0] +’ ‘+ states[1] +’ ‘+ states[2] +’. ');
-  tally++;
-  break;
-} else if(guessedState !== StatesVisited [0],[1],[2],[3]) {
-  alert('Nope! I\'ve never visited '+ guessedState +' ! Try again.');
-}
-o--
+  if(statesVisited.includes(guessedState)) {
+    var index = statesVisited.indexOf(guessedState);
+    statesVisited.splice(index);
+    var statesString = '';
+    for(var i = 0; i < statesVisited.length; i++) {
+      statesString = statesString + statesVisited[i] + '\n';
+    }
+    alert('Correct! The other states are:' + '\n' + statesString);
+    tally++;
+    break;
+  } else {
+    alert('Nope! I\'ve never visited '+ guessedState +' ! Try again.');
+  }
+  numGuesses--;
 }
 
-if(wrongState === false){
-  alert('Nope, I\'ve never visted those states!');
-}
 console.log('tally', tally);
 
-alert('Congratulations!' You got '+ score +' 'out of seven.');
+alert('Congratulations! You got '+ tally +' out of 7.');
